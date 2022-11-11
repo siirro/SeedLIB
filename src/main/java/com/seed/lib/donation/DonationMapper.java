000c8b0064bdc0021a585846006e3e38bb25e725
songjh11 <jhmelody91@gmail.com>
2022-11-11 17:49:03 +0900

    1111-1

---
+++ src/main/java/com/seed/lib/donation/DonationMapper.java
@@ -0,0 +1,8 @@
+package com.seed.lib.donation;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface DonationMapper {
+
+}
---
+++ src/main/java/com/seed/lib/hope/HopeMapper.java
@@ -0,0 +1,10 @@
+package com.seed.lib.hope;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface HopeMapper {
+
+	
+	
+}
