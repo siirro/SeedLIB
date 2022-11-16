2bcea723c3a3988371e42737318016fddffad98e
KOOHYO <zpfhzpfh112@naver.com>
2022-11-16 09:45:37 +0900

    어드민 메인 연결

--- src/main/java/com/seed/lib/admin/AdminController.java
+++ src/main/java/com/seed/lib/admin/AdminController.java
@@ -1,12 +1,14 @@
 package com.seed.lib.admin;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/admin/*")
 public class AdminController {
 
+	@GetMapping("main")
 	public String getMain()throws Exception{
 		return "admin/main";
 	}
