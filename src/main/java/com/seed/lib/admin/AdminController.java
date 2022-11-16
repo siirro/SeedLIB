2a28e324096f4e2dc5e5016230b22d6371f07076
KOOHYO <zpfhzpfh112@naver.com>
2022-11-16 09:37:59 +0900

    프로파일 수정

--- src/main/java/com/seed/lib/admin/AdminController.java
+++ src/main/java/com/seed/lib/admin/AdminController.java
@@ -1,5 +1,14 @@
 package com.seed.lib.admin;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin/*")
 public class AdminController {
 
+	public String getMain()throws Exception{
+		return "admin/main";
+	}
+	
 }
