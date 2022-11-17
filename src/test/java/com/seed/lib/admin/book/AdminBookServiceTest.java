feba6c08167bdebef15655627d5d8ee83d3165c0
KOOHYO <zpfhzpfh112@naver.com>
2022-11-17 15:52:19 +0900

    도서추가 테스트중

--- src/test/java/com/seed/lib/admin/book/AdminBookServiceTest.java
+++ src/test/java/com/seed/lib/admin/book/AdminBookServiceTest.java
@@ -9,19 +9,25 @@ import org.springframework.boot.test.context.SpringBootTest;
 import com.seed.lib.book.BookVO;
 
 @SpringBootTest
-class BookServiceTest {
+class AdminBookServiceTest {
 
 	@Autowired
 	private BookVO bookVO;
 	
 	@Autowired
-	private BookService bookService;
+	private AdminBookService adminBookService;
 	
 	@Test
 	void setBookAddtest() {
 		bookVO.setIsbn(1L);
 		bookVO.setTitle("title");
 		bookVO.setWriter("writer");
+		bookVO.setPublisher("출판사");
+		bookVO.setBookDate(2022L);
+		bookVO.setCategory(000L);
+		bookVO.setQuantity(1L);
+		bookVO.setImage("이미지");
+		bookVO.setBookC
 		
 	}
 
