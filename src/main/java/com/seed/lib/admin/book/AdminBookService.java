b5b92b1933ebe31661a2f7d0dd0233d19b1d5d54
songjh11 <jhmelody91@gmail.com>
2022-11-17 15:36:52 +0900

    1117

--- src/main/java/com/seed/lib/admin/book/AdminBookService.java
+++ src/main/java/com/seed/lib/admin/book/AdminBookService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 import com.seed.lib.book.BookVO;
 
 @Service
-public class BookService {
+public class AdminBookService {
 
 	@Autowired
 	private BookMapperIF bookMapperIF;
