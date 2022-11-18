255473ab5b57cfa4fe40076407d9858dbb552914
KOOHYO <zpfhzpfh112@naver.com>
2022-11-18 14:33:39 +0900

    프로그램 행사 추가 기능 구현중

--- src/main/java/com/seed/lib/admin/program/ProgramController.java
+++ src/main/java/com/seed/lib/admin/program/ProgramController.java
@@ -2,6 +2,7 @@ package com.seed.lib.admin.program;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,5 +11,10 @@ public class ProgramController {
 
 	@Autowired
 	private ProgramService programService;
+	
+	@GetMapping("add")
+	public String setProgramAdd()throws Exception{
+		return "";
+	}
 	
 }
