4480fee3bcb6271239a7ef42644b27a7c63e2e58
KOOHYO <zpfhzpfh112@naver.com>
2022-11-21 16:03:23 +0900

    프로그램 추가페이지 작업중

--- src/main/java/com/seed/lib/admin/program/ProgramController.java
+++ src/main/java/com/seed/lib/admin/program/ProgramController.java
@@ -3,6 +3,7 @@ package com.seed.lib.admin.program;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -14,6 +15,18 @@ public class ProgramController {
 	
 	@GetMapping("add")
 	public String setProgramAdd()throws Exception{
+		return "admin/program/add";
+	}
+	
+	@PostMapping("add")
+	public String setProgramAdd(ProgramVO programVO)throws Exception{
+		
+		int result = programService.setProgramAdd(programVO);
+		
+		if(result > 0) {
+			return "admin/program/list";
+		}
+		
 		return "admin/program/add";
 	}
 	
