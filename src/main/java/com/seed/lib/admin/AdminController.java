11d8da083b769e4cca27eebc1149dcbff09fc7df
KOOHYO <zpfhzpfh112@naver.com>
2022-11-25 12:46:36 +0900

    로그인 페이지 연결 완료!

--- src/main/java/com/seed/lib/admin/AdminController.java
+++ src/main/java/com/seed/lib/admin/AdminController.java
@@ -8,8 +8,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/admin/*")
 public class AdminController {
 
+	
+	// 어드민 로그인 페이지
+	@GetMapping("adLogin")
+	public String getAdLogin()throws Exception{
+		return "admin/adLogin";
+	}
+	
+	// 어드민 메인 페이지
 	@GetMapping("main")
-	public String getMain()throws Exception{
+	public String getAdMain()throws Exception{
 		return "admin/main";
 	}
 	
