7daa794c8997bc258a1cfb7099d9b5c718bb4ecf
KOOHYO <zpfhzpfh112@naver.com>
2022-12-05 15:30:21 +0900

    회원관리 매퍼 완료

--- src/main/java/com/seed/lib/admin/member/AdMemberMapper.java
+++ src/main/java/com/seed/lib/admin/member/AdMemberMapper.java
@@ -1,8 +1,15 @@
 package com.seed.lib.admin.member;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 
-@Mapper
-public class AdMemberMapper {
+import com.seed.lib.member.MemberVO;
 
+@Mapper
+public interface AdMemberMapper {
+
+	// 회원 목록 꺼내오기
+	public List<MemberVO> getAdMemberList()throws Exception;
+	
 }
